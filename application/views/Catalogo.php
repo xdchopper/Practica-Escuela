579614be48088c339b098d47a8d510bfec1846ae
Eduardo <cuecho_14@hotmail.com>
2016-09-19 15:34:33 -0600

    Tablas con bootstrap

--- application/views/Catalogo.php
+++ application/views/Catalogo.php
@@ -34,55 +34,64 @@
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </head>
 <body>
-	<?php echo validation_errors(); ?>
-	<form action="<?php echo site_url('/controlador/'.$accion.''); ?>" method="post">
-		<input type="hidden" name="id" value="<?php echo $this->uri->segment(3); ?>">
-		<table border="1px">
-			<tr>
-				<td><label for="lblNombre" name="lblNombre">Nombre :</label></td>
-				<td><input type="text" name="txtNombre" value="<?php echo $nombre; ?>"></td>
-			</tr>
-			<tr>
-				<td><label for="lblEdad" name="lblEdad">Edad :</label></td>
-				<td><input type="text" name="txtEdad" value="<?php echo $edad; ?>"></td>
-			</tr>
-			<tr>
-				<td><label for="lblSexo" name="lblSexo" value="<?php echo $sexo; ?>">Sexo :</label></td>
-				<td>
-					<select name="sltSexo" id="sltSexo">
-						<?php 
-					switch ($sexo) 
-					{
-						case '':
-						?>
-							<option value="" selected>Seleccione...</option>
-							<option value="M">Masculino</option>
-							<option value="F">Femenino</option>
-						<?php
-							break;
-						case 'M':
-						?>
-							<option value="0">Seleccione...</option>
-							<option value="M" selected>Masculino</option>
-							<option value="F">Femenino</option>
-						<?php
-							break;
-						case 'F':
-						?>
-							<option value="0">Seleccione...</option>
-							<option value="M" >Masculino</option>
-							<option value="F" selected>Femenino</option>
-						<?php
-							break;
-					}
-				 ?>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2"><input type="submit" name="btnAceptar" value="Aceptar"></td>
-			</tr>
-		</table>
-	</form>
+	<div class="container">
+		<?php echo validation_errors(); ?>
+		<div class="panel panel-default">
+				<div class="panel-heading">
+						ALUMNO
+				</div>
+				<div class="panel-body">
+					<form action="<?php echo site_url('/controlador/'.$accion.''); ?>" method="post">
+						<input type="hidden" name="id" value="<?php echo $this->uri->segment(3); ?>">
+						<table class="table table-striped table-hover table-bordered">
+							<tr>
+								<td><label for="lblNombre" name="lblNombre">Nombre :</label></td>
+								<td><input type="text" name="txtNombre" value="<?php echo $nombre; ?>"></td>
+							</tr>
+							<tr>
+								<td><label for="lblEdad" name="lblEdad">Edad :</label></td>
+								<td><input type="text" name="txtEdad" value="<?php echo $edad; ?>"></td>
+							</tr>
+							<tr>
+								<td><label for="lblSexo" name="lblSexo" value="<?php echo $sexo; ?>">Sexo :</label></td>
+								<td>
+									<select name="sltSexo" id="sltSexo">
+										<?php 
+									switch ($sexo) 
+									{
+										case '':
+										?>
+											<option value="" selected>Seleccione...</option>
+											<option value="M">Masculino</option>
+											<option value="F">Femenino</option>
+										<?php
+											break;
+										case 'M':
+										?>
+											<option value="0">Seleccione...</option>
+											<option value="M" selected>Masculino</option>
+											<option value="F">Femenino</option>
+										<?php
+											break;
+										case 'F':
+										?>
+											<option value="0">Seleccione...</option>
+											<option value="M" >Masculino</option>
+											<option value="F" selected>Femenino</option>
+										<?php
+											break;
+									}
+								 ?>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2"><input type="submit" name="btnAceptar" value="Aceptar"></td>
+							</tr>
+						</table>
+					</form>
+				</div>
+		</div>
+	</div>
 </body>
 </html>
--- application/views/index.php
+++ application/views/index.php
@@ -20,39 +20,52 @@
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
 </head>
 <body>
-	<?php echo validation_errors(); ?>
-	<?php echo form_open('controlador/read'); ?>
-	<form action="" method="post">
-		<table>
-			<tr>
-				<td><label for="lblBuscar" name="lblBuscar">Buscar : </label></td>
-				<td><input type="text" name="txtBuscar"></td>
-				<td><input type="submit" name="btnBuscar" value="Buscar"></td>
-				<td></td>
-				<td><input type="button" name="btnAgregar" value="Agregar" onclick="Agregar();"></td>
-			</tr>
-		</table>
-	</form>
-	</br>
-	<table border="1px">
-		<?php if($usuarios){ ?>
-			<tr>
-				<th><label for="lblID">ID</label></th>
-				<th><label for="lblNombre">Nombre</label></th>
-				<th><label for="lblEdad">Edad</label></th>
-				<th><label for="lblSexo">Sexo</label></th>
-			</tr>
-			<?php foreach ($usuarios as $usuario){ ?>
-				<tr>
-					<td><?php echo $usuario->ID ?></td>
-					<td><label for="lbl<?php echo $usuario->ID ?>" name="lbl<?php echo $usuario->ID ?>" value="<?php echo $usuario->Nombre ?>"><?php echo $usuario->Nombre ?></label></td>
-					<td><?php echo $usuario->Edad ?></td>
-					<td><?php echo $usuario->Sexo ?></td>
-					<td><a href="<?php echo site_url('/controlador/llenarCatalogo/'); ?><?php echo $usuario->ID ?>">Editar</a></td>
-					<td><a href="<?php echo site_url('/controlador/delete/'); ?><?php echo $usuario->ID ?>">Eliminar</a></td>
-				</tr>
-			<?php } ?>
-		<?php } ?>
-	</table>
+	<div class="container">
+		<?php echo validation_errors(); ?>
+		<?php echo form_open('controlador/read'); ?>
+		<div class="row">
+			<div class="col-sm-4">
+				<form action="" method="post">
+					<div class="input-group">
+						<input type="text" name="txtBuscar" class="form-control" placeholder="Buscar">
+						<div class="input-group-btn">
+							<input type="submit" name="btnBuscar" value="Buscar" class="btn btn-default">
+							<input type="button" name="btnAgregar" value="Agregar" onclick="Agregar();" class="btn btn-default">
+						</div>
+					</div>
+				</form>
+			</div>
+			<div class="col-sm-8"></div>
+		</div>
+		</br>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				ALUMNOS
+			</div>
+			<div class="panel-body">
+				<table class="table table-striped table-hover table-bordered">
+					<?php if($usuarios){ ?>
+						<tr style="background-color:#81BEF7">
+							<th><label for="lblID">ID</label></th>
+							<th><label for="lblNombre">Nombre</label></th>
+							<th><label for="lblEdad">Edad</label></th>
+							<th><label for="lblSexo">Sexo</label></th>
+							<th colspan="2" ><label>Opciones</label></th>
+						</tr>
+						<?php foreach ($usuarios as $usuario){ ?>
+							<tr>
+								<td><?php echo $usuario->ID ?></td>
+								<td><label for="lbl<?php echo $usuario->ID ?>" name="lbl<?php echo $usuario->ID ?>" value="<?php echo $usuario->Nombre ?>"><?php echo $usuario->Nombre ?></label></td>
+								<td><?php echo $usuario->Edad ?></td>
+								<td><?php echo $usuario->Sexo ?></td>
+								<td><a href="<?php echo site_url('/controlador/llenarCatalogo/'); ?><?php echo $usuario->ID ?>">Editar</a></td>
+								<td><a href="<?php echo site_url('/controlador/delete/'); ?><?php echo $usuario->ID ?>">Eliminar</a></td>
+							</tr>
+						<?php } ?>
+					<?php } ?>
+				</table>
+			</div>	
+		</div>
+	</div>
 </body>
 </html>
